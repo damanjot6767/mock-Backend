1b2a59b85f2171e31de7ff0f9ce7ae2c907d329f
damanjot6767 <damanjot6767@gmail.com>
2023-01-19 17:34:28 +0530

    add in it

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ require("dotenv").config()
 const connect = require('./src/connect/connect.js');
 const Auth = require("./src/routes/Auth.js");
 const UserTask = require('./src/routes/users.js');
-var app = express();
+const cors = require("cors")
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/",Auth);
 app.use("/user",UserTask)
